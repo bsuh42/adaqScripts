20d92288c2a69f6f797a0bb49bf46543439589e7
bsuh42 <bsuh42@gmail.com>
2019-09-23 13:48:23 -0400

    Add files via upload

--- fitSpectra.cpp
+++ fitSpectra.cpp
@@ -11,8 +11,16 @@ void spectra(TString file, Double_t lowerBound, Double_t upperBound)
   TH1D *h1 = new TH1D("", "", 4000, -1000, 3000);
   h1= (TH1D*)F->Get("hist");
 
-  TF1 *f1 = new TF1("f1", "gaus", lowerBound, upperBound);
-  h1->Fit(f1, "R");
+  TF1 *combinedFit = new TF1("combinedFit", "expo(0)+gaus(3)", lowerBound, upperBound);
+  TF1 *f1 = new TF1("f1", "expo", lowerBound, upperBound);
+  TF1 *f2 = new TF1("f2", "gaus", lowerBound, upperBound);
+  Double_t fitParameters[5];
+  h1->Fit(f1, "qR0");
+  h1->Fit(f2, "qR0");
+  f1->GetParameters(&fitParameters[0]);
+  f2->GetParameters(&fitParameters[2]);
+  combinedFit->SetParameters(fitParameters);
+  h1->Fit(combinedFit, "R");
 
   TCanvas *c1 = new TCanvas("c1", "", 1000, 600);
   gStyle->SetOptStat(0);
